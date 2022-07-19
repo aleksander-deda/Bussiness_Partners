a73fef8e4413cc88fb9a26274505841fcaa83024
Aleksander Deda <adeda@microfin.al>
2022-07-19 09:52:50 +0200

    simplifying the code

--- accountmanagers/urls.py
+++ accountmanagers/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from accountmanagers.views import account_manager_view, partners, add_partner, add_products_to_partners, underwriters
+from accountmanagers import views
 
 
 urlpatterns = [
-    path('', account_manager_view, name='account-manager'),
-    path('partners-list/', partners, name='partners-list'),
-    path('underwriters-list/', underwriters, name='underwriters-list'),
-    path('add-partners/', add_partner, name='add-partners'),
-    path('add-products-to-partners/<int:id>', add_products_to_partners, name='add-products-to-partners'),
+    path('', views.account_manager_dashboard, name='account-manager'),
+    path('my-profile/', views.MyProfile.as_view(), name='my_profile'),
+    path('partners-list/', views.partners, name='partners-list'),
+    path('underwriters-list/', views.underwriters, name='underwriters-list'),
+    path('add-partners/', views.add_partner, name='add-partners'),
+    path('add-products-to-partners/<int:id>', views.add_products_to_partners, name='add-products-to-partners'),
 
 
 ]
